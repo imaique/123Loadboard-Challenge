cd1548e339f5265217084862a65b610cfdba175c
Michael <osujim2012@gmail.com>
2023-11-17 20:55:17 -0500

    Create message_processor.py

---
+++ message_processor.py
@@ -0,0 +1,86 @@
+import json
+import time
+
+
+class Notifier:
+    def __init__(self) -> None:
+        pass
+
+
+class MessageProcessor:
+    def __init__(self) -> None:
+        self.notifier = Notifier()
+
+    def add_message(self, message: dict) -> None:
+        print(message["type"])
+
+
+def run_test_messages():
+    messages = [
+        {"seq": 0, "type": "Start", "timestamp": "2023-11-17T03:00:00.00123-05:00"},
+        {
+            "seq": 1,
+            "type": "Truck",
+            "timestamp": "2023-11-17T08:06:23.0406772-05:00",
+            "truckId": 114,
+            "positionLatitude": 41.425058,
+            "positionLongitude": -87.33366,
+            "equipType": "Van",
+            "nextTripLengthPreference": "Long",
+        },
+        {
+            "seq": 2,
+            "type": "Truck",
+            "timestamp": "2023-11-17T09:10:23.2531001-05:00",
+            "truckId": 346,
+            "positionLatitude": 39.195726,
+            "positionLongitude": -84.665296,
+            "equipType": "Van",
+            "nextTripLengthPreference": "Long",
+        },
+        {
+            "seq": 3,
+            "type": "Load",
+            "timestamp": "2023-11-17T11:31:35.0481646-05:00",
+            "loadId": 101,
+            "originLatitude": 39.531354,
+            "originLongitude": -87.440632,
+            "destinationLatitude": 37.639,
+            "destinationLongitude": -121.0052,
+            "equipmentType": "Van",
+            "price": 3150.0,
+            "mileage": 2166.0,
+        },
+        {
+            "seq": 4,
+            "type": "Load",
+            "timestamp": "2023-11-17T11:55:11.2311956-05:00",
+            "loadId": 201,
+            "originLatitude": 41.621465,
+            "originLongitude": -83.605482,
+            "destinationLatitude": 37.639,
+            "destinationLongitude": -121.0052,
+            "equipmentType": "Van",
+            "price": 3300.0,
+            "mileage": 2334.0,
+        },
+        {
+            "seq": 5,
+            "type": "Truck",
+            "timestamp": "2023-11-17T16:40:32.7200171-05:00",
+            "truckId": 114,
+            "positionLatitude": 40.32124710083008,
+            "positionLongitude": -86.74946594238281,
+            "equipType": "Van",
+            "nextTripLengthPreference": "Long",
+        },
+        {"seq": 6, "type": "End", "timestamp": "2023-11-17T22:52:21.1572422-05:00"},
+    ]
+    processor = MessageProcessor()
+    for message in messages:
+        processor.add_message(message)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    run_test_messages()
