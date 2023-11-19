0983c3d78f5277ea9a9eb75f98482bf1624925bf
SzmiSmi <97198979+SzmiSmi@users.noreply.github.com>
2023-11-19 02:33:28 -0500

    sorting trucks/loads/notifs

--- website/public/script.js
+++ website/public/script.js
@@ -1,138 +1,169 @@
 
-    // Map initialization 
-    var map = L.map('map').setView([41.425058, -87.33366], 5);
+// Map initialization 
+var map = L.map('map').setView([41.425058, -87.33366], 5);
 
-    //osm layer
-    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    osm.addTo(map);
+//osm layer
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+osm.addTo(map);
 
-    var marker, circle;
+var marker, circle;
 
-    fetch('/config').then(response => response.json()).then(config => {
-        const port = config.clientPort;
-        let socket = new WebSocket("ws://127.0.0.1:" + port);
+fetch('/config').then(response => response.json()).then(config => {
+    const port = config.clientPort;
+    let socket = new WebSocket("ws://127.0.0.1:" + port);
 
-        socket.addEventListener('open', function (event) {
-            console.log("Connected to WebSocket server");
-        });
-    
-        socket.addEventListener('message', function (event) {
-            console.log('Message from server ', event.data);
-            let jsonData = JSON.parse(event.data);
-            markPosition(jsonData)
-        });
+    socket.addEventListener('open', function (event) {
+        console.log("Connected to WebSocket server");
     });
 
-    var notificationList = []
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+        let jsonData = JSON.parse(event.data);
+        markPosition(jsonData)
+    });
+});
+
+function truckClick(truckId){
+    var truck = truckList[truckId];
+    document.getElementById("truck_id").innerHTML = truckId;
+    document.getElementById("truck_type").innerHTML = truck.equipType; 
     
-    function truckClick(truckId){
-        console.log(truckId);
-        document.getElementById("truck_id").innerHTML = truckId;
-        document.getElementById("truck_type").innerHTML = truckList[truckId].equipType; 
-        
-        // update notification list
-        var notificationList = document.getElementById("notification_list");
-        notificationList.innerHTML = "";
-        
-        if(truckList.length == 0) return;
-        // temporarily using trucks instead of notifications
-        truckList.forEach(function(truck){
-            var listItem = document.createElement("li");
-            listItem.textContent = truck.id;
-            notificationList.appendChild(listItem);
-        });
+    // update notification list
+    var notificationList = document.getElementById("notification_list");
+    notificationList.innerHTML = "";
+    
+    if(notificationForTruck.length == 0){
+        console.log("no notifications for truck " + truckId);
+        notificationList.appendChild(document.createElement("li").textContent = "No notifications for this truck");
+        return;
     }
+    // temporarily using trucks instead of notifications
+    for (const [key, value] in notificationForTruck.entries()){
+        var listItem = document.createElement("li");
+        listItem.textContent =  value.timestamp+ ": " + value.load; 
+        notificationList.appendChild(listItem);
+    }
+}
 
-    var truckList = []
-    var truckMarkerList = []
-    function markPosition(message){
-        // console.log(position)
-        if(message["type"]=="Truck"){
+var truckList = {}
+var truckMarkerList = {}
+var loadList = {}
+var notificationForTruck = {}
+var notificationForLoad = {}
 
-            var truckId = message["truckId"]
-            
-            var truck = truckList[truckId]
-            var lat = message["positionLatitude"]
-            if(lat == null) return;
-            var long = message["positionLongitude"]
-            
-            if(truck == null){
-                truckList[truckId] = new Truck(message)
-                var truckIcon = L.icon({
-                    iconUrl: "/icon_truck.png",
-                    iconSize: [38, 38]
-                });
-                marker = L.marker([lat, long], {
-                    icon: truckIcon,
-                    title: 'Truck '+truckId
-                    })
-                marker.on('click', truckClick.bind(this, truckId));
-                truckMarkerList[truckId] = marker
-                marker.addTo(map)
-            }else{
-                marker = truckMarkerList[truckId]
-                truck = truckList[truckId]
-                marker.setLatLng([lat, long])
-                truck.positionLatitude = lat
-                truck.positionLongitude = long 
-                truck.timestamp = new Date(message["timestamp"]).toLocaleDateString('en-US');
-                truck.nextTripLengthPreference = message["nextTripLengthPreference"];
-            }
-        }else if(message["type"]=="Load"){
-            // console.log("Load")
-            var lat = message["originLatitude"]
-
-            if(lat == null) return;
-            var long = message["originLongitude"]
-            
-            var loadIcon = L.icon({
-                iconUrl: "/icon_load.png",
+function markPosition(message){
+    // console.log(position)
+    if(message["type"]=="Truck"){
+        var truckId = message["truckId"]
+        
+        var truck = truckList[truckId]
+        var lat = message["positionLatitude"]
+        if(lat == null) return;
+        var long = message["positionLongitude"]
+        
+        if(truck === undefined){
+            truckList[truckId] = new Truck(message)
+            notificationForTruck[truckId] = []
+            var truckIcon = L.icon({
+                iconUrl: "/icon_truck.png",
                 iconSize: [38, 38]
             });
-
-            var accuracy = 200000
-            marker = L.marker([lat, long], {icon: loadIcon}) 
+            marker = L.marker([lat, long], {
+                icon: truckIcon
+                })
+            marker.on('click', truckClick.bind(this, truckId));
+            truckMarkerList[truckId] = marker
             marker.addTo(map)
-            // circle = L.circle([lat, long], {radius: accuracy})
-            // var featureGroup = L.featureGroup([marker, circle]).addTo(map)
-
         }else{
+            marker = truckMarkerList[truckId]
+            truck = truckList[truckId]
+            marker.setLatLng([lat, long])
+            truck.positionLatitude = lat
+            truck.positionLongitude = long 
+            truck.timestamp = new Date(message["timestamp"]).toLocaleDateString('en-US');
+            truck.nextTripLengthPreference = message["nextTripLengthPreference"];
+        }
+    }else if(message["type"]=="Load"){
+        var loadId = message["loadId"]
+        var lat = message["originLatitude"]
+        if(lat == null) return;
+        var long = message["originLongitude"]
+        
+        loadList[loadId] = new Load(message)
+        notificationForLoad[loadId] = []
+        // console.log("load added: "+loadList[loadId].id);
+        var loadIcon = L.icon({
+            iconUrl: "/icon_load.png",
+            iconSize: [38, 38]
+        });
+        marker = L.marker([lat, long], {
+            icon: loadIcon
+        })
+        // marker.on('click', function(){
+        //     document.getElementById("load_id").innerHTML = loadId;
+        //     document.getElementById("load_type").innerHTML = load.equipmentType; 
+        // });
+        marker.addTo(map)
+        // var accuracy = 200000
+        // circle = L.circle([lat, long], {radius: accuracy})
+        // var featureGroup = L.featureGroup([marker, circle]).addTo(map)
+
+    }else if(message["type"]=="Notification"){
+        var notification = new Notification(message)
+        var truckId = notification.truckId
+        var loadId = notification.loadId
+
+        if(notificationForTruck[truckId]){
+            notificationForTruck[truckId].push(notification);
+        }else{
+            console.error(`TruckId ${truckId} does not exist.`);
             return;
         }
+        if(notificationForLoad[loadId]){
+            notificationForLoad[loadId].push(notification)
+        }else{
+            console.error(`LoadId ${loadId} does not exist.`);
+            return;
+        }  
 
-
-        console.log("Your coordinate is: Lat: "+ lat +" Long: "+ long+ " Accuracy: "+ accuracy)
+    }else{
+        return;
     }
+    // console.log("Your coordinate is: Lat: "+ lat +" Long: "+ long)
+}//markPosition
 
-    class Truck {
-        constructor(message) {
-          this.id = message["truckId"];
-          this.timestamp = new Date(message["timestamp"]).toLocaleDateString('en-US');
-          this.positionLatitude = message["positionLatitude"];
-          this.positionLongitude = message["positionLongitude"];
-          this.equipType = message["equipType"];
-          this.nextTripLengthPreference = message["nextTripLengthPreference"];
-        }
-      }
-    class Load {
-        constructor(message) {
-            this.id = message["loadId"];
-            this.timestamp = new Date(message["timestamp"]).toLocaleDateString('en-US');
-            this.originLatitude = message["originLatitude"];
-            this.originLongitude = message["originLongitude"];
-            this.destinationLatitude = message["destinationLatitude"];
-            this.destinationLongitude = message["destinationLongitude"]
-            this.equipmentType = message["equipmentType"];
-            this.price = message["price"];
-            this.mileage = message["mileage"];
-          }
-      }
-      class Notification {
-        constructor(truckId, loadId, timestamp) {
-          this.truck = truckId;
-          this.load = loadId;
-          this.timestamp = timestamp;
-        }
-      }
+class Truck {
+    constructor(message) {
+        this.id = message["truckId"];
+        this.timestamp = new Date(message["timestamp"]).toLocaleDateString('en-US');
+        this.positionLatitude = message["positionLatitude"];
+        this.positionLongitude = message["positionLongitude"];
+        this.equipType = message["equipType"];
+        this.nextTripLengthPreference = message["nextTripLengthPreference"];
+    //   this.notifications = [];
+    }//constructor
+}//Truck
+class Load {
+    constructor(message) {
+        this.id = message["loadId"];
+        this.timestamp = new Date(message["timestamp"]).toLocaleDateString('en-US');
+        this.originLatitude = message["originLatitude"];
+        this.originLongitude = message["originLongitude"];
+        this.destinationLatitude = message["destinationLatitude"];
+        this.destinationLongitude = message["destinationLongitude"]
+        this.equipmentType = message["equipmentType"];
+        this.price = message["price"];
+        this.mileage = message["mileage"];
+        // console.log("load added: "+this.id);
+        // this.notifications = [];
+    }//constructor
+}//Load
+class Notification {
+    constructor(message) {
+      this.truckId = message["truck_id"];
+      this.loadId = message["load_id"];
+      this.timestamp = message["timestamp"];
+    }
+  }//notification
